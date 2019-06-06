55d0ddd8535923f9a266fa1fc4eb5ad47b1ea1a0
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-06-05 21:43:54 -0300

    Starting dashboard

--- app/containers/screens/Dashboard.js
+++ app/containers/screens/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Body } from 'native-base';
+import Avatar from '../../components/common/avatar';
 
 import { connect } from 'react-redux';
 
@@ -7,7 +9,18 @@ class Dashboard extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Dashboard</Text>
+        <Card style={styles.profileCard}>
+          <CardItem>
+            <Body>
+              <Avatar
+                size={100}
+                onPress={{}}
+                callback={uploaded => this.setState({ uploaded })}
+                style={{color: '#4D9BA3'}}
+              />
+            </Body>
+          </CardItem>
+        </Card>
       </View>
     );
   }
@@ -18,7 +31,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#AAD8DD'
+  },
+  profileCard: {
+    height: 130, 
+    marginTop: 200,
+    width: 350,
+    borderRadius: 20,
+    borderTopEndRadius: 20,
+    overflow: 'hidden',
     backgroundColor: '#4D9BA3'
   }
 });
