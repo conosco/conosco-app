5d1dc608dc642c0ae2e24e0764158a669ddb2013
Vitor Leal <vitinho2201@gmail.com>
2019-06-06 23:14:44 -0300

    UpdateRegister init
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>

---
+++ app/containers/screens/UpdateRegister.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+import Input from '../../components/common/input';
+
+class UpdateRegister extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
